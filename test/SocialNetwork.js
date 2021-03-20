3d13bf261ec7576ad961953184befbbf7c45a22c
Naofal Leo Agusta <naofal.leo.agusta98@gmail.com>
2021-03-20 10:51:17 +0700

    implemented test with mocha

---
+++ test/SocialNetwork.js
@@ -0,0 +1,23 @@
+const { assert } = require("chai");
+const _deploy_contracts = require("../migrations/2_deploy_contracts");
+
+const SocialNetwork = artifacts.require("./SocialNetwork.sol");
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+contract("SocialNetwork", (accounts) => {
+  let socialNetwork;
+
+  describe("deployment", () => {
+    it("deploy successfully", async () => {
+      socialNetwork = await SocialNetwork.deployed();
+      const address = await socialNetwork.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+  });
+});
